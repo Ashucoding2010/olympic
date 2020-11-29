55544354eab2be1d2e5ee9c728aeee62524b68f8
Ashucoding2010 <70793998+Ashucoding2010@users.noreply.github.com>
2020-11-29 13:05:53 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,45 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+var color = "red";
+ctx.beginPath();
+ctx.strokeStyle = color
+ctx.lineWidth = 2;
+ctx.arc(275, 200, 50, 0, 360)
+ctx.stroke()
+
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+var color = "orange";
+ctx.beginPath();
+ctx.strokeStyle = color
+ctx.lineWidth = 2;
+ctx.arc(350, 200, 50, 0, 360)
+ctx.stroke()
+
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+var color = "blue";
+ctx.beginPath();
+ctx.strokeStyle = color
+ctx.lineWidth = 2;
+ctx.arc(425, 200, 50, 0, 360)
+ctx.stroke()
+
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+var color = "green";
+ctx.beginPath();
+ctx.strokeStyle = color
+ctx.lineWidth = 2;
+ctx.arc(390, 275, 50, 0, 360)
+ctx.stroke()
+
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+var color = "yellow";
+ctx.beginPath();
+ctx.strokeStyle = color
+ctx.lineWidth = 2;
+ctx.arc(310, 275, 50, 0, 360)
+ctx.stroke()
+
